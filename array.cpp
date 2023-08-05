412f0ec59f5d97e106a0772b78640bb3d1959785
Aakar <aakaar.bh@gmail.com>
2023-08-05 17:35:46 +0530

    Array itteration example using for loop

---
+++ array.cpp
@@ -0,0 +1,13 @@
+
+//itteratio example using for loop
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int marks[4]={23,43,21,44};
+    for(int i=0;i<4;i++){
+        cout<<"student "<<i+1<<" ="<<marks[i]<<endl;
+    }
+    return 0;
+}
