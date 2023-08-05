a0bbcb09b3a9a04c943c2195a14c3adf999d3d66
Aakar <aakaar.bh@gmail.com>
2023-08-05 16:13:12 +0530

    Basic Cpp function

---
+++ func_add.cpp
@@ -0,0 +1,14 @@
+//basic addition funtion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void add(){
+    int a=5,b=16,sum=0;
+    sum=a+b;
+    cout<<"sum = "<<sum;
+}
+
+int main(){
+    add();
+}
