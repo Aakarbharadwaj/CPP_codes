b0481481d4621e6ca418a6d1f481c91dfa2078ea
Aakar <aakaar.bh@gmail.com>
2023-08-07 12:24:09 +0530

    function with pointer to add and subtract

---
+++ pointer_cal8.cpp
@@ -0,0 +1,26 @@
+// Calculate Sum and Difference:
+// Write a function that calculates both the sum and the difference of two integers 
+// using pointers and returns both values.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void calculate(int* p1,int* p2){
+    int sum=*p1+*p2;
+    int difference=*p2-*p1;
+    cout<<*p1<<"+"<<*p2<<"="<<sum<<endl;
+    cout<<*p2<<"-"<<*p1<<"="<<difference;
+
+    
+}
+
+int main(){
+    int a,b;
+    int* ptr1=&a;
+    int* ptr2=&b;
+    cout<<"enter value 1 :";
+    cin>>a;
+    cout<<"enter value 1 :";
+    cin>>b;
+    calculate(ptr1,ptr2);
+}
