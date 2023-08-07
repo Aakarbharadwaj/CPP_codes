e7c6be56cb2762265c2886f881973fbb2db5b319
Aakar <aakaar.bh@gmail.com>
2023-08-07 23:41:11 +0530

    declaring double pointer

---
+++ double_pointer1.cpp
@@ -0,0 +1,15 @@
+//1. Declare a pointer to an integer `num` and another pointer to a pointer pointing to `num`.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int num=20;
+	int* ptr=&num;
+	int** q=&ptr;
+	cout<<**q<<endl; //value of num
+	cout<<q<<endl; //address of ptr
+	cout<<&ptr<<endl; //address of ptr
+	cout<<&num<<endl;//address of num
+	cout<<*q; //address of num
+}
